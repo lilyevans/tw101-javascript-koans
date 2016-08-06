25a2c295b3c313f7f836844a95cd948d0be7f2a8
lilyevans <evanslm2@g.cofc.edu>
2016-08-05 19:58:51 -0500

    problem1

--- js/problem1.js
+++ js/problem1.js
@@ -29,6 +29,12 @@ function kiteGame(numberOfKites) {
     var answer = "";
 
     // write some code here!
+    for (var i = 1; i <= numberOfKites; i++ ){
+        if (i % 15 == 0) answer += "Peche! Ipo kaate! ";
+        else if (i % 3 == 0) answer += "Peche! ";
+        else if (i % 5 == 0) answer += "Ipo kaate! ";
+        else answer += i + " ";
+    }
 
     changeElementText("#answer", answer);
 }
